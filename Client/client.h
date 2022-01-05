fe847c95731d6964d91e727df9f0bfa2afec9166
Kristian Mazzucca <mazzuccakristian@gmail.com>
2022-01-05 18:47:07 +0100

    "GetTitle" and "StringFormatter" defined.

--- Client/client.h
+++ Client/client.h
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <pthread.h>
+#include <string>
 
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
+using std::getline;
 
 void ShowMenu();
 
 int GetUserInput();
+
+string GetTitle();
+
+string StringFormatter(string rawString);
